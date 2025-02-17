f717d69a649a3bda5bd36e4698262f6b81ac6586
Matyas.G <gebremedhinmatyas@gmail.com>
2025-02-17 15:41:05 +0300

    ** about page margin removed about
    ** HTMl selector update
    
    ** unwanted removal of width and height
    
    ** full customization of about page based on design
    
    **  some customization on me page
    
    ** some customization on login page removal of autofill background color removal
    
    ** customization on email and password field
    
    **  some addition of custom div in login html
    
    ** some addition of custom div on about html

--- custom_ethiotop/public/dist/js/custom-web.bundle.JKD47PAB.js
+++ custom_ethiotop/public/dist/js/custom-web.bundle.JKD47PAB.js
@@ -4,9 +4,10 @@
     let mainContainer = document.querySelector("main.container");
     mainContainer.classList.remove("container");
     mainContainer.classList.add("container-fluid");
-    var element = document.querySelector(".text-muted");
+    mainContainer.classList.remove("my-4");
+    var element = document.querySelector("a.text-muted");
     element.textContent = "Mehbub General Trading PLC";
     element.href = "https://mehbubgtplc.com";
   });
 })();
-//# sourceMappingURL=custom-web.bundle.FH4WKJMF.js.map
+//# sourceMappingURL=custom-web.bundle.JKD47PAB.js.map
--- custom_ethiotop/public/js/custom-web.js
+++ custom_ethiotop/public/js/custom-web.js
@@ -7,10 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
     mainContainer.classList.remove('container');
     mainContainer.classList.add('container-fluid');
 
-    // Assuming there's only one element with this class, or you're only interested in the first one
-var element = document.querySelector('.text-muted');
+    mainContainer.classList.remove('my-4');
 
-// Change the text inside the anchor
+    // Assuming there's only one element with this class, or you're only interested in the first one
+var element = document.querySelector('a.text-muted');
+
+
+// // Change the text inside the anchor
 element.textContent = 'Mehbub General Trading PLC';
 
 // Change the href attribute
